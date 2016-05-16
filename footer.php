be952860ce7b88943f7a8c96768711c73eb0baaa
rimeister <rimeister@hotmail.com>
2016-05-15 23:40:42 -0400

    Addded second loop for Photos on Home page

--- footer.php
+++ footer.php
@@ -17,9 +17,7 @@
 			<div class="site-info container">
 				<div class="row">
 					<?php if( !get_theme_mod('footer_social') ) activello_social_icons(); ?>
-					<div class="copyright col-md-12">
-						<?php echo esc_html( get_theme_mod( 'activello_footer_copyright', 'Activello' ) ); ?>
-						<?php activello_footer_info(); ?>
+						<p>All content is &copy; 2016 Riley Boyd.</p>
 					</div>
 				</div>
 			</div><!-- .site-info -->
--- page-photos.php
+++ page-photos.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template Name: Home page
+ * Template Name: Photos page
  *
  * This is the template that displays full width page without sidebar
  *
@@ -12,78 +12,10 @@ get_header(); ?>
 	<div id="primary" class="content-area">
         <?php $paged = (get_query_var('paged')) ? get_query_var('paged') : 1; ?>
 
-		<main id="main" class="site-main <?php echo "page-".$paged;?>" role="main">
-			<div class="article-container">
-				<h3>Latest Articles</h3>
-				<?php
-
-					$args = array(
-					    'posts_per_page' => 5,
-					    'order' => 'DESC'
-					);
-
-					$rp = new WP_Query( $args );
-
-					if($rp->have_posts()) :
-					    while($rp->have_posts()) : $rp->the_post();
-					?>
-
-					<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-						<div class="blog-item-wrap">
-							<div class="post-inner-content">
-								<header class="entry-header page-header">
-									<h1 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
-
-									<?php if ( 'post' == get_post_type() ) : ?>
-									<div class="entry-meta">
-										<?php activello_posted_on(); ?>
-
-										<?php
-											edit_post_link(
-												sprintf(
-													/* translators: %s: Name of current post */
-													esc_html__( 'Edit %s', 'activello' ),
-													the_title( '<span class="screen-reader-text">"', '"</span>', false )
-												),
-												'<span class="edit-link">',
-												'</span>'
-											);
-										?>
-
-									</div><!-- .entry-meta -->
-									<?php endif; ?>
-								</header><!-- .entry-header -->
-								<?php 
-							       if ( has_post_thumbnail() ) {  // check if the post has a Post Thumbnail assigned to it.
-							    ?>
-							       	<a href="<?php echo get_permalink() ?>">
-							       	<?php
-							          the_post_thumbnail(); //display the thumbnail
-							        ?>
-							    	</a>
-
-							    <?php } ?>
-							    <?php
-							       the_excerpt(); // displays the excerpt
-
-						       ?>
-
-								<div class="read-more">
-									<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php esc_html_e( 'Read More', 'activello' ); ?></a>
-								</div>		
-
-					   		</div>
-						</div>
-
-				<?php
-					endwhile;
-					    wp_reset_postdata(); // always always remember to reset postdata when using a custom query, very important
-					endif;
-				?>
-			</div>						
+		<main id="main" class="site-main <?php echo "page-".$paged;?>" role="main">				
 
 			<div class="article-container">
-				<h3>Latest Photos</h3>
+				<h3>Photos</h3>
 				<?php
 
 					$args = array(
