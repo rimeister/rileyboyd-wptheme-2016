bb99bdb784f8e2ae8f33cdc36532a2e012bcb6db
rimeister <rimeister@hotmail.com>
2016-06-05 20:34:26 -0400

    Making another contact form fix

--- forms/contactform.php
+++ forms/contactform.php
@@ -7,7 +7,7 @@
 	$to      = 'contact@rileyboyd.com';
 	$subject = 'Contact Form Message';
 	$headers = "From: $name\r\nReply-to: $email";
-	$message = 'From: $name <'.$email.'>'.\r\n.$message;
+	$message = 'From: '.$name.' <'.$email.'>'.\r\n.$message;
 	mail($to, $subject, $message, $headers);
 
 ?>
