a11fda824ebf866075f2c907682abb3a1389e0a9
Riley Boyd <contact@rileyboyd.com>
2017-09-17 16:06:45 -0400

    Trying to fix WP jquery noConflict bug

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+jQuery(document).ready(function($){
 
 	if ( $('#gallery').length ) {
 
@@ -16,54 +16,54 @@ $(document).ready(function(){
 	});
 	feed.run();
 
-});
+	$('.contact-form').validate({
 
-$('.contact-form').validate({
-
-	rules: {
-     // no quoting necessary
-	    name: {
-	    	required: true,
-	    },
-	    email: {
-	    	required: true,
-	    	email: true
-	    },
-	    message: {
-	    	required: true
-	    }
-	},
-
-	messages: {
-		name: "Please enter your name",
-		email: {
-			required: "Please enter your email address",
-			email: "Please enter a valid email address (e.g., name@example.com)"
+		rules: {
+	     // no quoting necessary
+		    name: {
+		    	required: true,
+		    },
+		    email: {
+		    	required: true,
+		    	email: true
+		    },
+		    message: {
+		    	required: true
+		    }
 		},
-		message: "Please enter a message"
-	},
 
-    submitHandler: function(form) {
-        $.ajax({
-        	type: 'POST',
-            url: 'http://rileyboyd.com/wp-content/themes/rileyboyd-wptheme-2016/forms/contactform.php',
-            data: $(form).serialize(),
-            success: function(response) {
-				$('#contact-confirm-modal').modal();          
-			}            
-        });
-    }
+		messages: {
+			name: "Please enter your name",
+			email: {
+				required: "Please enter your email address",
+				email: "Please enter a valid email address (e.g., name@example.com)"
+			},
+			message: "Please enter a message"
+		},
+
+	    submitHandler: function(form) {
+	        $.ajax({
+	        	type: 'POST',
+	            url: 'http://rileyboyd.com/wp-content/themes/rileyboyd-wptheme-2016/forms/contactform.php',
+	            data: $(form).serialize(),
+	            success: function(response) {
+					$('#contact-confirm-modal').modal();          
+				}            
+	        });
+	    }
+
+	});
+
+	// Avoid spam
+	var datCom = 'LmNvbQ=='
+	var datWord = 'RW1haWw=';
+	var datDomane = 'cmlsZXlib3lk'
+	var userReachout = 'Y29udGFjdA==';
+	var datSymb = 'QA==';
+	var datSned = atob(userReachout)+atob(datSymb)+atob(datDomane)+atob(datCom);
+	 
+	var reachoutBox = '<div class="row reachouttext"><div class="col-sm-10 col-sm-push-1"><p>'+atob(datWord)+': <a href="mailto:'+datSned+'" target="_blank">'+datSned+'</a></p><p>Or, use this form. It all goes to the same place.</p></div></div>';
+	$('.contact-content').prepend(reachoutBox);
 
 });
-
-// Avoid spam
-var datCom = 'LmNvbQ=='
-var datWord = 'RW1haWw=';
-var datDomane = 'cmlsZXlib3lk'
-var userReachout = 'Y29udGFjdA==';
-var datSymb = 'QA==';
-var datSned = atob(userReachout)+atob(datSymb)+atob(datDomane)+atob(datCom);
- 
-var reachoutBox = '<div class="row reachouttext"><div class="col-sm-10 col-sm-push-1"><p>'+atob(datWord)+': <a href="mailto:'+datSned+'" target="_blank">'+datSned+'</a></p><p>Or, use this form. It all goes to the same place.</p></div></div>';
-$('.contact-content').prepend(reachoutBox);
 
